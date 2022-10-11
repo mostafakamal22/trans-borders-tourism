ed01623ef9a96686774786ba0efa34fb125820c8
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-10-11 21:34:14 +0200

    fix adding svg issue

--- frontend/src/components/shared/NotFoundPage.tsx
+++ frontend/src/components/shared/NotFoundPage.tsx
@@ -1,13 +1,16 @@
 import { AiFillHome } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { ReactComponent as NotFoundSVG } from "../../assets/imgs/Trans-404.svg";
 
 const NotFoundPage = () => {
   return (
     <div className="min-h-screen w-full flex justify-center items-center p-10 md:px-20 md:py-10">
-      <div className="max-w-5xl w-full flex flex-col justify-center items-center p-6 bg-amber-50 rounded shadow-lg shadow-black/30 border-y-4 border-amber-800">
+      <div className="max-w-5xl w-full flex flex-col justify-center items-center p-6 bg-amber-50 rounded shadow-lg shadow-black/30 border-y-4 border-red-800">
         <h2 className="text-2xl md:text-3xl text-center font-bold text-blue-900 mb-3">
           Page Not Found
         </h2>
+
+        <NotFoundSVG />
 
         <Link
           to="/"
