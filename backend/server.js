9fa2970bf9faf555b91d67b63e866ecd81684a28
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-10-12 20:43:38 +0200

    fix path name#5

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,12 @@ app.use("/api/invoices", invoicesRoute);
 
 //serve Frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("dist/trans-borders-tourism")));
+  app.use(express.static(path.join("frontend/dist/trans-borders-tourism")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve("dist", "trans-borders-tourism", "index.html"))
+    res.sendFile(
+      path.resolve("frontend", "dist", "trans-borders-tourism", "index.html")
+    )
   );
 }
 
