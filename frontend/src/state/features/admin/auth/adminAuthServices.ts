74881bc98efcc4961a75e6dfebef9b0f23e04aff
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-10-12 22:03:40 +0200

    fix api path issue

--- frontend/src/state/features/admin/auth/adminAuthServices.ts
+++ frontend/src/state/features/admin/auth/adminAuthServices.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? `${process.env.CORS_DOMAINS}/api/admins/`
+    ? "https://cute-gold-bison-tie.cyclic.app/api/admins/"
     : "http://localhost:5000/api/admins/";
 
 //Login Admin
