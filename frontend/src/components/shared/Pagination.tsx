55a76192e76d419e992a4eac305fbc0b74b718e0
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-11-23 23:32:27 +0200

    edit tables pagination functionalites and build#35

--- frontend/src/components/shared/Pagination.tsx
+++ frontend/src/components/shared/Pagination.tsx
@@ -1,27 +1,158 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { TiChevronLeft, TiChevronRight } from "react-icons/ti";
 
-const Pagination = ({ range, setPage, page, slice }: any) => {
+type PaginationProps = {
+  range: number[];
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  page: number;
+  slice: any;
+};
+
+const Pagination = ({ range, setPage, page, slice }: PaginationProps) => {
+  const [pageQuery, setPageQuery] = useState<number>(1);
+
   useEffect(() => {
     if (slice.length < 1 && page !== 1) {
       setPage(page - 1);
     }
   }, [slice, page, setPage]);
+
   return (
-    <nav className="mx-auto my-5 text-center">
-      <ul className="inline-flex items-center gap-1 flex-wrap mx-2">
-        {range.map((el: any, index: number) => (
-          <li key={index} onClick={() => setPage(el)}>
+    <div className="flex items-center justify-center flex-wrap gap-4 my-10">
+      {/* Show Page Count */}
+      <div className="text-sm font-semibold">
+        <span>{`عرض الصفحة  (${page}) من (${range.length})`}</span>
+      </div>
+
+      {/* Table Pages */}
+      <nav className="text-center">
+        <ul className="flex items-center gap-1 flex-wrap mx-2">
+          {/* Go To Prev Page Button*/}
+          <li>
             <button
-              className={`${
-                el === page && "bg-red-100"
-              } min-w-[40px] p-2 leading-tight text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black`}
+              title={`${page !== 1 ? "Previous Page" : ""}`}
+              onClick={() => setPage(page - 1)}
+              disabled={page === 1}
+              className="flex justify-center items-center min-w-[40px] p-2 leading-tight text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black disabled:cursor-not-allowed disabled:bg-gray-200 disabled:border-gray-500 disabled:text-black"
             >
-              {el}
+              <TiChevronLeft size={20} />
             </button>
           </li>
-        ))}
-      </ul>
-    </nav>
+
+          {/* More Previous Pages To Show && First Page Button */}
+          {range.length > 5 && page > 5 && (
+            <>
+              <li onClick={() => setPage(range[0])}>
+                <button
+                  title={`Go First Page`}
+                  className="p-2 leading-tight text-sm text-red-900 font-semibold border border-red-300 rounded hover:bg-red-100 hover:text-black"
+                >
+                  {"الصفحة الأولى"}
+                </button>
+              </li>
+
+              <li className="min-w-[40px] flex justify-center items-center mr-1 p-2 leading-tight font-bold text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black">
+                {"...."}
+              </li>
+            </>
+          )}
+
+          {/* Case OF Table Pages <= 5 Pages*/}
+          {range.length <= 5 &&
+            range.map((pageNumber: number) => (
+              <li key={pageNumber} onClick={() => setPage(pageNumber)}>
+                <button
+                  title={`Go Page ${pageNumber}`}
+                  className={`${
+                    pageNumber === page && "bg-red-100"
+                  } min-w-[40px] p-2 leading-tight text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black`}
+                >
+                  {pageNumber}
+                </button>
+              </li>
+            ))}
+
+          {/* Case OF Table Pages > 5 Pages*/}
+          {range.length > 5 &&
+            range
+              .slice(
+                page < range.length - 4 ? page - 1 : page - 5,
+                page < range.length - 4 ? page + 4 : page
+              )
+              .map((pageNumber: number) => (
+                <li key={pageNumber} onClick={() => setPage(pageNumber)}>
+                  <button
+                    title={`Go Page ${pageNumber}`}
+                    className={`${
+                      pageNumber === page && "bg-red-100"
+                    } min-w-[40px] p-2 leading-tight text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black`}
+                  >
+                    {pageNumber}
+                  </button>
+                </li>
+              ))}
+
+          {/* More Next Pages To Show && Last Page Button */}
+          {range.length > 5 && page < range.length && (
+            <>
+              <li className="min-w-[40px] flex justify-center items-center p-2 ml-1 leading-tight font-bold text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black">
+                {"...."}
+              </li>
+
+              <li onClick={() => setPage(range.length)}>
+                <button
+                  title={`Go Last Page`}
+                  className="p-2 leading-tight text-sm text-red-900 font-semibold border border-red-300 rounded hover:bg-red-100 hover:text-black"
+                >
+                  {"الصفحة الأخيرة"}
+                </button>
+              </li>
+            </>
+          )}
+
+          {/* Go To Next Page Button*/}
+          <li>
+            <button
+              title={`${page !== range.length ? "Next Page" : ""}`}
+              onClick={() => setPage(page + 1)}
+              disabled={page === range.length}
+              className="flex justify-center items-center min-w-[40px] p-2 leading-tight text-red-900 border border-red-300 rounded hover:bg-red-100 hover:text-black disabled:cursor-not-allowed disabled:bg-gray-200 disabled:border-gray-500 disabled:text-black"
+            >
+              <TiChevronRight size={20} />
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Show Page Count */}
+      <div className="max-w-[200px] flex flex-row-reverse justify-center items-center flex-wrap gap-2 text-sm font-semibold">
+        <label htmlFor="goPage">اختر الصفحة</label>
+        <input
+          className="max-w-[80px] p-2 bg-red-100 border border-red-500 text-center rounded focus:outline-none focus:border-blue-700"
+          type={"number"}
+          name="goPage"
+          min={1}
+          max={range.length}
+          value={pageQuery}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setPageQuery(+e.target.value);
+          }}
+        />
+        <div className="basis-full">
+          <button
+            className="bg-blue-800 px-4 py-2 text-white font-semibold border rounded hover:border-blue-700 hover:bg-white hover:text-blue-700"
+            type="button"
+            onClick={() => {
+              if (range.includes(pageQuery) && page !== pageQuery) {
+                setPage(pageQuery);
+              }
+            }}
+          >
+            ذهاب للصفحة
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
