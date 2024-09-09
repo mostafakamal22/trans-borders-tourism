dd4146ccd7d83464fbccc6ad840ca03829da074f
mostafakamal22 <mk1319@fayoum.edu.eg>
2024-09-09 23:07:23 +0300

    customize chunking behavior in the vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -122,3 +122,121 @@ const App = () => {
 };
 
 export default App;
+
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import { Suspense, lazy } from "react";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.min.css";
+
+// // Lazy loaded components
+// const AdminLogin = lazy(() => import("./components/forms/AdminLogin"));
+// const NotFoundPage = lazy(() => import("./components/shared/NotFoundPage"));
+// const AdminProfile = lazy(() => import("./components/profile/AdminProfile"));
+// const Home = lazy(() => import("./components/home/Home"));
+// const Invoices = lazy(() => import("./components/invoice/Invoices"));
+// const ShowInvoice = lazy(() => import("./components/invoice/ShowInvoice"));
+// const UpdateAdmin = lazy(() => import("./components/forms/UpdateAdmin"));
+// const Passports = lazy(() => import("./components/passport/Passports"));
+// const CreatePassport = lazy(() => import("./components/forms/CreatePassport"));
+// const CreateTicket = lazy(() => import("./components/forms/CreateTicket"));
+// const Tickets = lazy(() => import("./components/ticket/Tickets"));
+// const Purchases = lazy(() => import("./components/purchase/Purchases"));
+// const CreatePurchase = lazy(() => import("./components/forms/CreatePurchase"));
+// const Payments = lazy(() => import("./components/payment/Payments"));
+// const CreatePayment = lazy(() => import("./components/forms/CreatePayment"));
+// const Bills = lazy(() => import("./components/bill/Bills"));
+// const CreateBill = lazy(() => import("./components/forms/CreateBill"));
+// const ShowBill = lazy(() => import("./components/bill/ShowBill"));
+// const ReceiptVoucher = lazy(() => import("./components/receiptVoucher/ReceiptVoucher"));
+// const CreateReceiptVoucher = lazy(() => import("./components/forms/CreateReceiptVoucher"));
+// const ReceiptVouchers = lazy(() => import("./components/receiptVoucher/ReceiptVouchers"));
+// const PaymentVouchers = lazy(() => import("./components/paymentVoucher/PaymentVouchers"));
+// const CreatePaymentVoucher = lazy(() => import("./components/forms/CreatePaymentVoucher"));
+// const PaymentVoucher = lazy(() => import("./components/paymentVoucher/PaymentVoucher"));
+// const PublicLayout = lazy(() => import("./components/shared/PublicLayout"));
+// const MainLayout = lazy(() => import("./components/shared/MainLayout"));
+// const PersistLogin = lazy(() => import("./components/shared/PersistLogin"));
+// const RequireAuth = lazy(() => import("./components/shared/RequireAuth"));
+
+// const App = () => {
+//   return (
+//     <>
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <Routes>
+//           <Route path="/">
+//             {/* Public Routes */}
+//             <Route element={<PublicLayout />}>
+//               <Route index element={<Navigate to={"/login"} />} />
+//               <Route path="login" element={<AdminLogin />} />
+//             </Route>
+
+//             {/* Protected Routes */}
+//             <Route element={<PersistLogin />}>
+//               <Route element={<RequireAuth />}>
+//                 <Route element={<MainLayout />}>
+//                   <Route path="/home" element={<Home />} />
+//                   {/* Admin Setting Routes */}
+//                   <Route path="/profile">
+//                     <Route index element={<AdminProfile />} />
+//                     <Route path="update" element={<UpdateAdmin />} />
+//                   </Route>
+//                   {/* Passports Routes */}
+//                   <Route path="/passports">
+//                     <Route index element={<Passports />} />
+//                     <Route path="create" element={<CreatePassport />} />
+//                   </Route>
+//                   {/* Invoices Routes */}
+//                   <Route path="/invoices">
+//                     <Route index element={<Invoices />} />
+//                     {/* <Route path="create" element={<CreateInvoice />} /> */}
+//                     <Route path=":id" element={<ShowInvoice />} />
+//                   </Route>
+//                   {/* Bills Routes */}
+//                   <Route path="/bills">
+//                     <Route index element={<Bills />} />
+//                     <Route path="create" element={<CreateBill />} />
+//                     <Route path=":id" element={<ShowBill />} />
+//                   </Route>
+//                   {/* Tickets Routes */}
+//                   <Route path="/tickets">
+//                     <Route index element={<Tickets />} />
+//                     <Route path="create" element={<CreateTicket />} />
+//                   </Route>
+//                   {/* Purchases Routes */}
+//                   <Route path="/purchases">
+//                     <Route index element={<Purchases />} />
+//                     <Route path="create" element={<CreatePurchase />} />
+//                   </Route>
+//                   {/* Payments Routes */}
+//                   <Route path="/payments">
+//                     <Route index element={<Payments />} />
+//                     <Route path="create" element={<CreatePayment />} />
+//                   </Route>
+//                   {/* Receipt Vouchers Routes */}
+//                   <Route path="/receiptVouchers">
+//                     <Route index element={<ReceiptVouchers />} />
+//                     <Route path="create" element={<CreateReceiptVoucher />} />
+//                     <Route path=":id" element={<ReceiptVoucher />} />
+//                   </Route>
+//                   {/* Payment Vouchers Routes */}
+//                   <Route path="/paymentVouchers">
+//                     <Route index element={<PaymentVouchers />} />
+//                     <Route path="create" element={<CreatePaymentVoucher />} />
+//                     <Route path=":id" element={<PaymentVoucher />} />
+//                   </Route>
+//                 </Route>
+//               </Route>
+//             </Route>
+
+//             {/* Not Found Route */}
+//             <Route path="*" element={<NotFoundPage />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+
+//       <ToastContainer role="alert" rtl={true} autoClose={4000} theme="colored" />
+//     </>
+//   );
+// };
+
+// export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,19 @@ export default defineConfig({
     "process.env": process.env,
   },
   plugins: [svgr(), react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
