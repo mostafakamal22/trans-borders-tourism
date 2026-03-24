b77baca3140e5b5b907a1959f626bd697f3738e3
mostafakamal22 <mk1319@fayoum.edu.eg>
2026-03-25 01:53:01 +0200

    add errors logs file

--- backend/server.ts
+++ backend/server.ts
@@ -11,6 +11,21 @@ import { adminApiLimiter } from "./middlewares/adminMiddlewares/adminApiLimiter"
 import { connectToMongoose } from "./config/dbConfig";
 import { corsDevOptions, corsProOptions } from "./config/corsConfig";
 
+//Erorrs Logs
+import fs from "fs";
+
+const log = (msg: any) => {
+  fs.appendFileSync("error.log", JSON.stringify(msg) + "\n");
+};
+
+process.on("uncaughtException", (err) => {
+  log(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  log(err);
+});
+
 // Solve Mongodb connection error
 import dns from "node:dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
